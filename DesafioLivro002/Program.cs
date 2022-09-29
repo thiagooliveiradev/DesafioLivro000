56b9157e39b38c1a60aa9bbef21fa0d55cef33ca
Thiago Oliveira <thiagooliveiradat@gmail.com>
2022-09-29 16:04:01 -0300

    sistema de calculo de salario após promoção.

---
+++ DesafioLivro002/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário, sabendo-se
+que este sofreu um aumento de 25% */
+
+Console.Write("Digite o valor do salário atual do funcionário: ");
+var salarioAtual = Console.ReadLine();
+int novoSalario = int.Parse(salarioAtual);
+
+var promocao = novoSalario + novoSalario * 25 / 100;
+
+Console.WriteLine($"Parabéns pela promoção seu novo salario é: {promocao}");
