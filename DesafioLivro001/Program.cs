0742c1189c28d1c620f31039e20b73aaedfc44a7
Thiago Oliveira <thiagooliveiradat@gmail.com>
2022-09-29 15:48:44 -0300

    criação do sistema que calcula amedia do aluno.

---
+++ DesafioLivro001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Faça um programa que receba três notas, calcule e mostre a média aritmética
+
+
+Console.Write("Digite a nota da primeira avaliação do aluno: ");
+var notaPrimeiraAvaliacao = Console.ReadLine();
+int resultadoDaAvaliacao1 = int.Parse(notaPrimeiraAvaliacao);
+
+Console.Write("Digite a nota da segunda avaliação do aluno: ");
+var notaSegundaAvalicao = Console.ReadLine();
+int resultadoDaAvaliacao2 = int.Parse(notaSegundaAvalicao);
+
+Console.Write("Digite a nota da terceira avaliação do aluno: ");
+var notaTerceiraAvaliacao = Console.ReadLine();
+var resultadoDaAvaliacao3 = int.Parse(notaTerceiraAvaliacao);
+
+var media = (resultadoDaAvaliacao1 + resultadoDaAvaliacao2 + resultadoDaAvaliacao3) / 3;
+
+if (media < 6)
+{
+    Console.WriteLine($"O resultado da média do aluno é {media}, REPROVADO!");
+}
+
+else if (media >= 6)
+{
+    Console.WriteLine($"O resultado da média do aluno é {media}, APROVADO!");
+}
+
