6c2bccf3987f66a70a87f310d106789f405513dd
thiag <thiag@DESKTOP-05CBJ0V>
2022-09-29 14:14:43 -0300

    Add project files.

---
+++ DesafioLivro000/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite o valor da primeira entrada:");
+var entradaDoUsuario1 = Console.ReadLine();
+int valorDigitadoUsuario1 = int.Parse(entradaDoUsuario1);
+Console.Write("Digite o valor da segunda entrada:");
+var entradaDoUsuario2 = Console.ReadLine();
+int valorDigitadoUsuario2 = int.Parse(entradaDoUsuario2);
+Console.Write("Digite o valor da terceira entrada:");
+var entradaDoUsuario3 = Console.ReadLine();
+int valorDigitadoUsuario3 = int.Parse(entradaDoUsuario3);
+
+
+var soma = valorDigitadoUsuario1 + valorDigitadoUsuario2 + valorDigitadoUsuario3;
+
+Console.WriteLine(soma);
