b37585b3a5ba84512e4c325d5d99aaea6eee4a28
Thiago Oliveira <59004790+thiagooliveiradev@users.noreply.github.com>
2022-09-30 02:54:22 -0300

    Feature/tirando espaços blocos condicionais (#4)
    * alteração feita no codigo para aceitar numeros decimais.
    
    * Retirado os espaços entre os blocos condicionais
    
    * Programa para saber quando anos a pessoal terá em 2050 apartir do ano de nascimento.

---
+++ DesafioLivro005/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Faça um programa que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre: 
+a) a idade dessa pessoa;
+b) quantos anos ela terá em 2050.*/ 
+
+
+Console.Write("Digite o ano que você nasceu, para saber quantos anos terá em 2050: ");
+var dataNascimento = Console.ReadLine();
+var anoNascimento = int.Parse(dataNascimento);
+
+var dataEntrada = DateTime.Now.Year;
+var Acrescentaridade = (2050 - dataEntrada);
+var idadeFutura = dataEntrada - anoNascimento + Acrescentaridade;
+
+Console.WriteLine($"Sua idade em 2050 será: {idadeFutura}");
+
+
