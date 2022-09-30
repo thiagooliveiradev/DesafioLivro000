c4da6c32bdf7f9bd2b4b52509dc99d6b3324a502
Thiago Oliveira <thiagooliveiradat@gmail.com>
2022-09-30 14:54:18 -0300

    Consultar preseça de alunos da lista

---
+++ DesafioLivro006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite o nome do aluno para consultar a presença dele na aula de hoje: ");
+var aluno = Console.ReadLine();
+
+switch (aluno) 
+{
+    case "Marcelo": Console.WriteLine("Presente!"); break;
+    case "Renato": Console.WriteLine("Presente!"); break;
+    case "Thiago": Console.WriteLine("Presete!"); break;
+    case "Paulo": Console.WriteLine("Atestado médico"); break;
+    case "Flavio": Console.WriteLine("Faltou");break;
+    default: Console.WriteLine("Aluno não encontrado"); break;        
+      
+}
+
